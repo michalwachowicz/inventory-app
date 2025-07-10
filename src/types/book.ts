9d09605a4fa9aef9863f989a287929f839a4414f
michalwachowicz <wachowiczmichal1@gmail.com>
2025-07-10 10:56:17 +0200

    feat: create a book type

---
+++ src/types/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+  id: number;
+  isbn: string;
+  title: string;
+  description: string;
+  cover: string;
+  publicationYear: number;
+  pages: number;
+  authorId: number;
+  genreId: number;
+}
