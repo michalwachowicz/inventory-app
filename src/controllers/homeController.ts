8c1c459660981cec1c72999b1bf15a91517ebb52
michalwachowicz <wachowiczmichal1@gmail.com>
2025-07-10 12:16:00 +0200

    feat: add home render options type

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
 import { getRecentlyAddedBooks } from "../db/queries";
 import { renderView } from "../utils/viewRenderer";
+import { HomeRenderOptions } from "../types/render-options";
 
 export async function getHome(_: Request, res: Response) {
-  const results = await getRecentlyAddedBooks();
+  const recentlyAddedBooks = await getRecentlyAddedBooks();
 
-  await renderView(res, {
+  await renderView<HomeRenderOptions>(res, {
     title: "Home",
     viewName: "home",
     navbar: "full",
-    results,
+    recentlyAddedBooks,
   });
 }
--- src/types/render-options.ts
+++ src/types/render-options.ts
@@ -4,5 +4,14 @@ export interface RenderOptions {
   viewName: string;
   title?: string;
   navbar?: "basic" | "full";
-  results?: Book[];
+  selectedGenreId?: string;
+}
+
+export interface HomeRenderOptions extends RenderOptions {
+  recentlyAddedBooks: Book[];
+}
+
+export interface ResultsRenderOptions extends RenderOptions {
+  results: Book[];
+  pages: number;
 }
