d8a07c4585417c9caa123f418ad2af68fd4bc978
michalwachowicz <wachowiczmichal1@gmail.com>
2025-07-22 09:50:31 +0200

    chore: add express json support

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
