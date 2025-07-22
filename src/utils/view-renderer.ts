06a2758b384baf78b35beaea24a56fb4e5fb42e8
michalwachowicz <wachowiczmichal1@gmail.com>
2025-07-22 12:11:59 +0200

    fix: cannot find genre module error

--- src/utils/view-renderer.ts
+++ src/utils/view-renderer.ts
@@ -1,14 +1,14 @@
 import { Response } from "express";
-import { Genre } from "../types/genre";
 import { getGenres } from "../db/queries";
 import { RenderOptions, SuccessRenderOptions } from "../types/render-options";
 import { capitalize } from "./capitalize";
+import { Entity } from "../types/entity";
 
 export async function renderView<T extends RenderOptions = RenderOptions>(
   res: Response,
   options: T,
 ) {
-  const genres: Genre[] = options.navbar === "full" ? await getGenres() : [];
+  const genres: Entity[] = options.navbar === "full" ? await getGenres() : [];
   res.render("index", { genres, ...options });
 }
 
